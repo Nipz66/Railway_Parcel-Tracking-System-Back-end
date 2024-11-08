dc76b3bb08c296e1cc2282f695271018ad5f948d
Nipun Anupama <anupamaalahakoon66@gmail.com>
2024-11-08 05:34:54 +0530

    crossorigin

--- src/main/java/edu/icet/controller/ParcelController.java
+++ src/main/java/edu/icet/controller/ParcelController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/parcel")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ParcelController {
     @Autowired
     final ParcelService service;
